b803cb304b6842a1e4e3433988ac804f4cfca804
David Agu <david.agu@iisysgroup.com>
2023-06-22 08:54:45 +0100

    Lesson 4,5 React Hooks and Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import CounterExample from './components/CounterExample';
 
 function App() {
   return (
     <div>
-      <HelloWorld name="David" expression="Wow"/>
+      <HelloWorld message="There, Welcome." expression="Wow"/>
+
+      <CounterExample/>
     </div>
   );
 }
