9cfba65344ef5e88c6dc851ab0b236314fd55931
David Agu <david.agu@iisysgroup.com>
2023-07-03 16:14:49 +0100

    Menu fixed and Add react-spring

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,35 +1,40 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useTransition, animated } from "react-spring";
 
-
+// menu className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
+// mask "bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
 
 function Navigation(){
-    const [showMenu, setShowMenu] = useState(false)
-    let menu 
-    let menuMask
+    const [showMenu, setShowMenu] = useState(false);
+    const menuTransitions =   useTransition(showMenu, {
+        from: {opacity: 0, left: '-100px'},
+        enter:{opacity: 1, left: '0px'},
+        leave: {opacity: 0, left: '-100px'},
+    });
 
-    if(showMenu){
-        menu = 
-        <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"> 
-        This is the menu
-        </div>
-        menuMask = <div
-        className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
-        onClick={()=>setShowMenu(false)}>
-
-        </div>
+    const toggleMenu = () =>{
+        setShowMenu(!showMenu);
     }
+
     return(
         <nav>
             <span className="text-xl">
                 <FontAwesomeIcon 
                 icon={faBars} 
-                onClick={() => setShowMenu(!showMenu)}
+                onClick={toggleMenu}
                 />
             </span>
-            { menuMask }
-            { menu }
+            {
+                menuTransitions((style, item) =>
+                       item ?  (<animated.div
+                        style={style}
+                        className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
+                        >
+                        THis is the menu
+                    </animated.div>) : null
+            )}
         </nav>
     )
 }
