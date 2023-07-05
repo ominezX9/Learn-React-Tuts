6390f18402806fd384cf782554549180d641bc0b
David Agu <david.agu@iisysgroup.com>
2023-07-05 14:29:11 +0100

    Introduction to Routing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,49 @@ import Header from "./Components/Header";
 // import './App.css';
 import HelloWorld from "./Components/HelloWorld";
 
+// React router imports
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom"
+
+function Home(){
+  return <h1 className="">This is the homepage.</h1>;
+}
+
+function About(){
+  return <h1 className="">This is the About page.</h1>;
+}
+
 function App(){
   return(
     <div>
       <Header/>
       <HelloWorld/>
+      <Router>
+          <ul>
+            <li>
+              <Link to='/' className="text-blue-500">Home</Link>
+            </li>
+            <li>
+              <Link to='/about' className="text-blue-500">About</Link>
+            </li>
+          </ul>
+          {/* The below changes the content displayed */}
+
+
+          <Routes>
+            <Route path='/' element={<Home/>}>
+            </Route>
+            <Route path="/about" element={<About/>}>
+
+            </Route>
+          </Routes>
+        
+      </Router>
+
       <Footer/>
     </div>
   )
--- src/Components/HelloWorld.js
+++ src/Components/HelloWorld.js
@@ -1,14 +1,28 @@
+// import { render } from "@testing-library/react";
 import React from "react";
 
 
-class HelloWorld extends React.Component{
-    render(){
-        return(
-            <div className="p-5 text-2xl">
-                <h1 className="font-bold">This is the homepage</h1>
-            </div>
-        )
-    }
+// class HelloWorld extends React.Component{
+//     render(){
+//         return(
+//             <div className="p-2 text-2xl">
+//                 <h1 className="font-bold">This is the homepage</h1>
+//                 {this.props.name}
+//             </div>
+//         )
+//     }
     
+// }
+function UserName(props){
+    return <h1>Hello, {props.name} </h1>
+}
+
+function HelloWorld(){
+    return(
+        <div className="p-2">
+            <h1 className="font-bold text-2xl">This is the homepage</h1>
+            <UserName name="David"/>
+        </div>
+    )
 }
 export default HelloWorld;
