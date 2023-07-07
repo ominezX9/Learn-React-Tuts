3583de8de430dde9a3a678d51ab38bbc46754ce4
David Agu <david.agu@iisysgroup.com>
2023-07-07 17:11:16 +0100

    Add Loader Component and Error Catch Method

---
+++ src/Components/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loader(){
+    return(
+        <div class='flex justify-center'>
+            <div className="loader"></div>
+        </div>
+    )
+}
+
+export default Loader;
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,39 +1,68 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import { useParams } from "react-router-dom";
+import Loader from "../Components/Loader";
 
 function Product(){
-    const { id  } = useParams();
+    const { id } = useParams();
     const url = `https://64a7bbe2dca581464b84add6.mockapi.io/api/v1/products/${id}`;
-    const [product, setProduct] = useState(null);
+    const [product, setProduct] = useState({
+        loading: false,
+        data: null,
+        error: false
+    });
 
     let content = null;
 
     useEffect(()=>{
+        //before request is sent
+        setProduct({
+            loading: true,
+            data: null,
+            error: false
+
+        })
         axios.get(url).then(response => {
-            setProduct(response.data);
-        });
+            setProduct({
+                loading: false,
+                data: response.data,
+                error: false
+            });
+        })
+        .catch(() => {
+            setProduct({
+                loading: false,
+                data: null,
+                error: true
+            })
+        })
     }, [url]);
+    if(product.error){
+        content = <p className="font-bold flex justify-center items-center">
+        There was an error please refresh or try again later
+    </p>
+    }
+    if(product.loading){
+        content=<Loader/>
+    }
 
-
-
-    if(product){
+    if(product.data){
         content = 
         <div>
             <h1 className="text-2xl font-bold mb-3">
-                {product.name}
+                {product.data.name}
             </h1>
             <div>
                 <img
-                src={product.image}
-                    alt={product.name}
+                src={product.data.image}
+                    alt={product.data.name}
                 />
             </div>
             <div className="font-bold text-xl mb-3">
-                $ {product.price}
+                $ {product.data.price}
             </div>
             <div>
-                {product.description}
+                {product.data.description}
             </div>
         </div>
     }
