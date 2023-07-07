9779f9fc44dee7be0f8efe64871288ad551711f3
David Agu <david.agu@iisysgroup.com>
2023-07-07 08:47:24 +0100

    Setup Products Page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Header from "./Components/Header";
 import Home from "./Views/Home";
 import About from "./Views/About";
 import ContactUs from "./Views/ContactUs";
+import Product from "./Views/Product";
 
 
 
@@ -31,6 +32,8 @@ function App(){
               </Route>
               <Route path="/about" element={<About/>}></Route>
               <Route path="/contact-us" element={<ContactUs/>}></Route>
+              <Route path="/products/:id" element={<Product/>}></Route>
+
 
             </Routes>
           </div>
---
+++ src/Views/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Product(){
+    return(
+        <div>
+            <h1>The product page</h1>
+        </div>
+    )
+}
+
+export default Product;
